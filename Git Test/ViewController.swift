1507ba94e3af478551a08d0b5ab29718e0128e0b
P. Lam <phillam@philips-mac-mini.local>
2018-09-09 22:15:45 -0400

    fixed extra "

--- Git Test/ViewController.swift
+++ Git Test/ViewController.swift
@@ -16,7 +16,7 @@ class ViewController: UIViewController {
     }
     
     @IBAction func buttonClicked(_ sender: UIButton) {
-        print("You Finished"")
+        print("You Finished")
     }
 
     override func didReceiveMemoryWarning() {
